a275d4edd0050858ba7af4604dbd31b154a32e47
Renan Silva <renansantos.silva04@outlook.com>
2022-11-04 08:55:58 -0300

    Aprendendo a requisitar URLs

--- worldmeters/worldmeters/spiders/countries.py
+++ worldmeters/worldmeters/spiders/countries.py
@@ -13,7 +13,8 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield {
-                'country_name': name,
-                'country_link': link
-            }
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
+
+            yield response.follow(url=link)
