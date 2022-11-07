f12b46c6d551c25a05a5fd050d2eccd695fb1297
Renan Silva <renansantos.silva04@outlook.com>
2022-11-07 09:03:27 -0300

    lógica para ir em outra página

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -15,3 +15,6 @@ class SpecialOffersSpider(scrapy.Spider):
                 'discounted_price': product.xpath('.//div[@class="p_box_price"]/span[1]/text()').get(),
                 'original_price': product.xpath('.//div[@class="p_box_price"]/span[2]/text()').get(),
             }
+        next_page = response.xpath('//a[@class="nextPage"]/@href').get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
