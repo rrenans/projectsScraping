a39baf508c3bda7646dc2c28663618b2e1087d7f
Renan Silva <renansantos.silva04@outlook.com>
2022-11-08 11:17:11 -0300

    Entendo sobre outras estruturas de spiders, ainda tenho algumas dúvidas, mas irei fazr o exercício e provavelmente vou resolver elas

---
+++ imdb/imdb/spiders/best_movies.py
@@ -0,0 +1,37 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['web.archive.org']
+    start_urls = ['']
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://web.archive.org/web/20200715000935/https://www.imdb.com/search/title/?groups=top_250&sort=user_rating', headers={
+            'User-Agent': self.user_agent
+        })
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//h3[@class="lister-item-header"]/a'), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths='(//a[contains(@class, "next-page")])[2]'), process_request='set_user_agent')
+    )
+
+    def set_user_agent(self, request, spider):
+        request.headers['User-Agent'] = self.user_agent
+        return request
+
+    def parse_item(self, response):
+        yield {
+            'title': response.xpath('//div[@class="title_wrapper"]/h1/text()').get(),
+            'year': response.xpath('//span[@id="titleYear"]/a/text()').get(),
+            # 'duration': response.xpath('//div[@class="subtext"]/time/text()').get().strip(),
+            'duration': response.xpath('normalize-space((//time)[1]/text())').get(),
+            'genre': response.xpath('//div[@class="subtext"]/a[1]/text()').get(),
+            'rating': response.xpath('//span[@itemprop="ratingValue"]/text()').get(),
+            'movie_url': response.url,
+            'user-agent': response.request.headers['User-Agent']
+        }
