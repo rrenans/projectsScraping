35238cd82541982bbb5c9cb854b8f7a8534fe46f
Renan Silva <renansantos.silva04@outlook.com>
2022-11-08 08:29:59 -0300

    Aprendendo a debugar

--- worldmeters/worldmeters/spiders/countries.py
+++ worldmeters/worldmeters/spiders/countries.py
@@ -1,4 +1,6 @@
 import scrapy
+# from scrapy.shell import inspect_response
+# from scrapy.utils.response import open_in_browser
 import logging
 
 # https://www.worldometers.info/world-population/honduras-population/
@@ -10,26 +12,30 @@ class CountriesSpider(scrapy.Spider):
 
     def parse(self, response):
         # title = response.xpath("//h1/text()").get()
-        countries = response.xpath("//td/a")
-        for country in countries:
-            name = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
+        # countries = response.xpath("//td/a")
+        # for country in countries:
+        #     name = country.xpath(".//text()").get()
+        #     link = country.xpath(".//@href").get()
 
             # absolute_url = f"https://www.worldometers.info{link}"
             # absolute_url = response.urljoin(link)
             # yield scrapy.Request(url=absolute_url)
 
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
+        yield response.follow(url='https://www.worldometers.info/world-population/china-population/', callback=self.parse_country, meta={'country_name': "China"})
 
     def parse_country(self, response):
+        logging.info(response.status)
+        logging.warning(response.status)
+        # open_in_browser(response)
+        # inspect_response(response, self)
         # logging.info(response.url)
-        name = response.request.meta['country_name']
-        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
-        for row in rows:
-            year = row.xpath(".//td[1]/text()").get()
-            population = row.xpath(".//td[2]/strong/text()").get()
-            yield {
-                'country_name': name,
-                'year': year,
-                'population': population,
-            }
+        # name = response.request.meta['country_name']
+        # rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        # for row in rows:
+        #     year = row.xpath(".//td[1]/text()").get()
+        #     population = row.xpath(".//td[2]/strong/text()").get()
+        #     yield {
+        #         'country_name': name,
+        #         'year': year,
+        #         'population': population,
+        #     }
