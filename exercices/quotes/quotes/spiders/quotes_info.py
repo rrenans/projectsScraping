0f9b9ce1b111d2da7152b8ccf43ef25da47e0f07
Renan Silva <renansantos.silva04@outlook.com>
2022-11-11 10:39:38 -0300

    Até o momento, estou conseguindo pegar os dados da primeira página, mas não estou conseguindo ir para o next

--- exercices/quotes/quotes/spiders/quotes_info.py
+++ exercices/quotes/quotes/spiders/quotes_info.py
@@ -1,10 +1,38 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class QuotesInfoSpider(scrapy.Spider):
     name = 'quotes_info'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/js/']
+
+    script = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            splash:set_viewport_full()
+            return {
+                image = splash:png(),
+                html = splash:html(),
+            }
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url='http://quotes.toscrape.com/js/', callback=self.parse, endpoint='execute', args={
+            'lua_source': self.script
+        })
 
     def parse(self, response):
-        pass
+        for quotes in response.xpath('//div[@class="quote"]'):
+            yield {
+                'quote_text': quotes.xpath('.//span[@class="text"]/text()').get(),
+                'author': quotes.xpath('.//span[2]/small/text()').get(),
+                'tags': quotes.xpath('.//div[@class="tags"]/a/text()').getall(),
+            }
+        
+        # next_page = response.xpath('//li[@class="next"]/a/@href').get()
+        # if next_page is not None:
+        #     yield scrapy.Request(url=next_page, callback=self.parse)
+        # yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
