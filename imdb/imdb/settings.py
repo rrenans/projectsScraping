6a493ebb52709d17fc2adb3cd0b1330778d66b74
Renan Silva <renansantos.silva04@outlook.com>
2022-11-28 09:54:46 -0300

    Aprendendo pipelines

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -62,9 +62,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'imdb.pipelines.ImdbPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'imdb.pipelines.ImdbPipeline': 300,
+   # pasta . arquivo . classse . prioridade
+#    'imdb.pipelines.FilterDuplicate': 100,
+}
+
+MONGO_URI = "Hello World"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
