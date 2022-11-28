d159824be614723b4648fee2dd87bfe82fbb0459
Renan Silva <renansantos.silva04@outlook.com>
2022-11-28 10:59:02 -0300

    adicionando https

--- cooking/cooking/spiders/recipe.py
+++ cooking/cooking/spiders/recipe.py
@@ -4,7 +4,7 @@ import scrapy
 class RecipeSpider(scrapy.Spider):
     name = 'recipe'
     allowed_domains = ['cooknenjoy.com']
-    start_urls = ['http://cooknenjoy.com/']
+    start_urls = ['https://cooknenjoy.com/']
 
     def parse(self, response):
         pass
