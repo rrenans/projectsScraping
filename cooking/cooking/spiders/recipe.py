51e2fed861a84ce1e3831c4a550b5caa8d048305
Renan Silva <renansantos.silva04@outlook.com>
2022-11-28 11:00:24 -0300

    Informações para seguir no desenvolvimento do projeto

--- cooking/cooking/spiders/recipe.py
+++ cooking/cooking/spiders/recipe.py
@@ -1,6 +1,31 @@
 import scrapy
 
 
+"""
+1- Pegar receitas
+    1.a - Info:
+        Nome Receita, (str)
+        Descrição, (str)
+        Avaliação, (str)
+        Tempo de Preparo, (str - num)
+        Tempo de Fogão/Forno, (str - num)
+        Tempo Total, (str - num)
+        Categoria, (str)
+        Cozinha, (str)
+        Porções, (str - num)
+        Calorias, (str - num)
+    1.b - Info 2:
+        Ingredientes, (list)
+        Modo de preparo (str)
+        Dicas, (str)
+        URL, (str)
+        Img, (ing)
+
+2- Mexer em Settings, Middlewares e Pipelines (conectar com um banco)
+3- Tentar utilizar Selenium/Splash
+4- Enviar para e-mail
+"""
+
 class RecipeSpider(scrapy.Spider):
     name = 'recipe'
     allowed_domains = ['cooknenjoy.com']
