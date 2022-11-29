4775859e73c48026dcfc1f5b4ecead43644556e0
Renan Silva <renansantos.silva04@outlook.com>
2022-11-29 10:07:05 -0300

    Login com scrapy

---
+++ demo_logging/demo_logging/spiders/openlibrary_login.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy import FormRequest
+
+class OpenlibraryLoginSpider(scrapy.Spider):
+    name = 'openlibrary_login'
+    allowed_domains = ['openlibrary.org']
+    start_urls = ['https://openlibrary.org/account/login']
+
+    def parse(self, response):
+        yield FormRequest.from_response(
+            response,
+            formid='register',
+            formdata={
+                'username': 'renansantos.silva04@outlook.com',
+                'password': 'Familia2011',
+                'redirect': '/',
+                'debug_token': '',
+                 'login': 'Log In',
+            },
+            callback=self.after_login
+        )
+    def after_login(self, response):
+        print('logged in...')
